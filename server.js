680ea2d17bca388bf59177de220dc8de123def97
SeakKimhour <seakkimhour20@kit.edu.kh>
2022-02-13 23:34:50 +0700

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 var cors=require('cors')
 const vehicleRoutes = require('./src/vehicle/routes')
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.use(express.json());
 app.use(cors())
