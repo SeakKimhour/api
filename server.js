d8f8d076293638b5745aa8926dccb053f19ef7ee
SeakKimhour <seakkimhour20@kit.edu.kh>
2022-02-13 23:32:23 +0700

    first commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = 4000;
 app.use(express.json());
 app.use(cors())
 
+
 app.use('/api/v1/vehicle', vehicleRoutes)
 
 app.listen(port, () => {
--- src/vehicle/routes.js
+++ src/vehicle/routes.js
@@ -1,7 +1,52 @@
 const {Router}=require('express');
 const controller=require('./controller');
 const routes =Router();
+const pool = require('../../db');
+const bcrypt = require('bcrypt');
+const jwtGenerator = require('../../utils/jwtGenerator');
+const validInfo= require('../../middleware/validInfo');
+const authorization= require('../../middleware/authorization');
 
+//jwt
+routes.post('/login',validInfo,async (req, res) => {
+    try {
+       
+       //1. destructure the req.body
+
+       const {username, user_password}=req.body;
+
+       //2. check if user doesn't exist (if not then we throw error)
+
+       const user=await pool.query("SELECT * FROM users WHERE users_name=$1 ",[username])
+       if (user.rows.length==0) {
+           return res.status(401).json("Password or Username does not incorrect...!!!")
+       }
+
+       //3.check if incoming password is the same the database password
+
+       const validPassword=await bcrypt.compare(user_password,user.rows[0].users_password);
+
+       if(!validPassword) {
+           return res.status(401).json("Password or username incorrect")
+       }
+       //4. give them the jwt token
+
+       const token =jwtGenerator(user.rows[0].user_id)
+       res.json({token});
+
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("Server error")
+    }
+})
+routes.get('/is-verify', authorization,async(req, res) => {
+   try {
+       res.json(true);
+   } catch (error) {
+       console.err(err.message);
+       res.status(500).send("Server error")
+   }
+})
 routes.get('/',controller.getVehicle );
 routes.get('/:id',controller.getVehicleById)
 routes.post('/',controller.addVehicle)
